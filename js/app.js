668e927328d64ad4a0b5a99500f0c18d3eb05f85
Mohamed Guerroui <guerroui.m@gmail.com>
2020-10-24 16:09:14 +0200

    découverte de vuejs

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log("coucou")
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      link: 'https://vuejs.org/v2/guide/',
+      success: true,
+      persons: ['Mohamed', 'Lionel', 'Eric', 'Jean', "Sarah"]
+    },
+    methods: {
+        close: function() {
+            this.success = false
+        }
+    }
+  })
