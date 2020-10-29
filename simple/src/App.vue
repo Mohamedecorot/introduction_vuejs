60b7f08e4d831f1fda1c4b43bce0a33874d2c328
Mohamed Guerroui <guerroui.m@gmail.com>
2020-10-29 16:40:22 +0100

    Vue-CLI

---
+++ simple/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import counter from './Counter.vue'
+
+export default {
+  components: { counter },
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
