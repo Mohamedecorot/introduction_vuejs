284d2a16f37f1ed0fdb117f2f0baca7fabd4ce6a
Mohamed Guerroui <guerroui.m@gmail.com>
2020-10-30 16:58:47 +0100

    animation et transition (dernière partie)

--- simple/src/Alert.vue
+++ simple/src/Alert.vue
@@ -1,34 +1,40 @@
 <template>
   <div>
     <button @click="toggle">Toggle</button>
-    <transition :css="false" @enter="enter" @leave="leave">
-        <div class="ui message success" v-if="visable">
+    <transition name="bounce" appear mode="out-in">
+        <div class="ui message success" v-if="visable" key="visable">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit recusandae
         nesciunt quia illo, omnis molestias ratione ullam ipsam blanditiis, totam
         reiciendis mollitia ut esse incidunt ducimus non magni impedit quaerat.
         </div>
-    </transition>
+        <div class="ui message success" v-else key="invisable">
+        ipsum dolor sit amet consectetur adipisicing elit. Odit recusandae
+        nesciunt quia illo, omnis molestias ratione ullam ipsam blanditiis, totam
+        reiciendis mollitia ut esse incidunt ducimus non magni impedit quaerat.
+        </div>
+    </transition> 
+    <transition-group name="bounce" tag="ul">
+        <li v-for="item in items" :key="item">{{ item }}</li>
+    </transition-group>
+    <button class="ui button" @click="add">Ajouter</button>
+    <button class="ui button error" @click="shuffle">Mélanger</button>
   </div>
 </template>
 <script>
-var slides = {
-    methods: {
-        enter: function (el, done) {
-            $(el).hide().slideDown(500, done)
-        },
-        leave: function (el, done) {
-            $(el).show().slideUp(500, done)
-        },
-    }
-}
 export default {
-    mixins: [slides],
     data() {
         return {
             visable: true,
-        };
+            items: [1,2,3,4,5]
+        }
     },
     methods: {
+        add: function() {
+            this.items.push(Math.random())
+        },
+        shuffle: function() {
+            this.items = [2,3,5,4,6]
+        },
         toggle: function() {
             this.visable = !this.visable;
         },
@@ -42,6 +48,9 @@ export default {
     .fade-enter, .fade-leave-active {
         opacity: 0;
         transform: translateX(20px);
+    }
+    .bounce-move{
+        transition: transform 10s;
     }
     .bounce-enter-active {
         animation: bounce-in .5s;
